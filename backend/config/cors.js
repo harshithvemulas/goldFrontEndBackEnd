28f846461b001020070326f0429b63057e7bb25d
Harshith Vemula <harshithvemulas@gmail.com>
2025-10-05 13:49:10 -0500

    updated cors policy
    Signed-off-by: Harshith Vemula <harshithvemulas@gmail.com>

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -1,8 +1,11 @@
 import { defineConfig } from '@adonisjs/cors';
 const corsConfig = defineConfig({
     enabled: true,
-    origin: true,
-    methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
+    origin: [
+        'https://goldpe.app',
+        'https://www.goldpe.app',
+    ],    
+    methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     headers: true,
     exposeHeaders: [
         'cache-control',
